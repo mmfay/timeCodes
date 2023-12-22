0912514c8ea96e9d9f8957fea05dda47042a144e
mmfay <mmfay3@gmail.com>
2023-12-21 19:23:56 -0800

    Enabled checkboxes for active/inactive

--- action_page.php
+++ action_page.php
@@ -1,3 +1,84 @@
 <?php
     echo $_POST["user"]
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>timeCodes</title>
+</head>
+<style>
+table, th, td {
+  border:1px solid black;
+  width: 50%;
+}
+.timecodes {
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 20%;
+}
+</style>
+<body>
+    <table class="timecodes">
+        <tr>
+            <th>Time Code</th>
+            <th>Description</th>
+            <th>Active</th>
+        </tr>
+        <?php
+        $servername = "localhost:3306";
+        $username1 = "root";
+        $password1 = "vaXjev98";
+        $dbname = "TIMECODES";
+        $conn = new mysqli($servername,$username1,$password1,$dbname);
+        if ($conn->connect_error) {
+            die("connection failed: " . $conn->connect_error);
+        }
+
+        // create select string
+        $sql = "SELECT TIMECODE, DESCRIPTION, ISACTIVE FROM TIMECODES ORDER BY TIMECODE DESC, ISACTIVE ASC;";
+
+        // prepare/execute statement
+        $result = $conn->query($sql); 
+
+        // loop through results and build sidenav list/links
+        if($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()){
+                echo "<tr><td>" . $row["TIMECODE"] . "</td><td>" . $row["DESCRIPTION"] . "</td>";
+                if ($row["ISACTIVE"] == 1) {
+                    echo "<td><input id='" . $row["TIMECODE"] . "' type='checkbox' onclick='updateActive(" . "\"" . $row["TIMECODE"] . "\"" . ")' checked /></td></tr>";
+                } else {
+                    echo "<td><input id='" . $row["TIMECODE"] . "' type='checkbox' onclick='updateActive(" . "\"" . $row["TIMECODE"] . "\"" . ")'/></td</tr>";
+                }
+            }
+        } 
+
+	
+    ?>
+    </table>
+    <script>
+        function updateActive(test) {
+            var active = "";
+            if (document.getElementById(test).checked) {
+                active = "1";
+            } else {
+                active = "0";
+            }
+            var postData = {
+            title: "TimeCode",
+            name: test,
+            isActive: active
+            };
+         var xhr = new XMLHttpRequest();
+         xhr.open("POST", "test.php", true);
+         xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+         xhr.send(JSON.stringify(postData));
+         xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && xhr.status === 201) {
+
+            } else {
+
+            }
+         };
+        }
+        </script>
+</body>
