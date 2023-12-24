6adc8950c93d5fec5c5107a016a1fa0471aeca60
mmfay <mmfay3@gmail.com>
2023-12-23 18:04:54 -0800

    Return Table of Codes

--- clientReq.php
+++ clientReq.php
@@ -1,19 +1,17 @@
 <?php 
     function insertRecord($code, $desc) {
-        $servername = "localhost:3306";
-        $username1 = "root";
-        $password1 = "vaXjev98";
-        $dbname = "TIMECODES";
-        $conn = new mysqli($servername,$username1,$password1,$dbname);
-        if ($conn->connect_error) {
-            die("connection failed: " . $conn->connect_error);
-        }
-        $sql = $conn->prepare("INSERT INTO TIMECODES (TIMECODE, DESCRIPTION, ISACTIVE) VALUES ('" . $code . "','" . $desc . "',1);");
-        // execute
-        $sql->execute();
-        $conn->close();
+        $query = "INSERT INTO TIMECODES (TIMECODE, DESCRIPTION, ISACTIVE) VALUES ('" . $code . "','" . $desc . "',1);";
+        executeSQL($query);
+        returnTable();
     }
     function updateRecord($code, $active) {
+        $query = "UPDATE TIMECODES SET ISACTIVE = " . intval($active) . " WHERE TIMECODE = '" . $code . "';";
+        executeSQL($query);
+    }
+    function deleteRecord() {
+        
+    }
+    function executeSQL($sqlQuery) {
         $servername = "localhost:3306";
         $username1 = "root";
         $password1 = "vaXjev98";
@@ -23,11 +21,42 @@
             die("connection failed: " . $conn->connect_error);
         }
         // prepare statement
-        $sql = $conn->prepare("UPDATE TIMECODES SET ISACTIVE = " . intval($active) . " WHERE TIMECODE = '" . $code . "';");
+        $sql = $conn->prepare($sqlQuery);
         //$sql = $conn->prepare("UPDATE TIMECODES SET ISACTIVE = 0 WHERE TIMECODE = '100A';";
         // execute
         $sql->execute();
         $conn->close();
+    }
+    function returnTable() {
+        $tableHeaders = "<tr><th>Time Code</th><th>Description</th><th>Active</th></tr>";
+        $servername = "localhost:3306";
+        $username1 = "root";
+        $password1 = "vaXjev98";
+        $dbname = "TIMECODES";
+        $conn = new mysqli($servername,$username1,$password1,$dbname);
+        if ($conn->connect_error) {
+            die("connection failed: " . $conn->connect_error);
+        }
+
+        // create select string
+        $sql = "SELECT TIMECODE, DESCRIPTION, ISACTIVE FROM TIMECODES ORDER BY TIMECODE ASC, ISACTIVE ASC;";
+
+        // prepare/execute statement
+        $result = $conn->query($sql); 
+
+        // loop through results and build sidenav list/links
+        if($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()){
+                $tableRow = $tableRow . "<tr><td>" . $row["TIMECODE"] . "</td><td>" . $row["DESCRIPTION"] . "</td>";
+                if ($row["ISACTIVE"] == 1) {
+                    $tableRow = $tableRow . "<td><input id='" . $row["TIMECODE"] . "' type='checkbox' onclick='updateActive(" . "\"" . $row["TIMECODE"] . "\"" . ")' checked /></td></tr>";
+                } else {
+                    $tableRow = $tableRow . "<td><input id='" . $row["TIMECODE"] . "' type='checkbox' onclick='updateActive(" . "\"" . $row["TIMECODE"] . "\"" . ")'/></td</tr>";
+                }
+            }
+        } 
+        $table = $tableHeaders . $tableRow;
+        echo $table;
     }
 // Retrieve the raw POST data
 $jsonData = file_get_contents('php://input');
@@ -40,36 +69,27 @@ if ($data !== null) {
    //$active = $data['isActive'];
    // Perform further processing or respond to the request
         $type = $data['title'];
-        if (strcmp($type, "Update") == 0) {
-            $name = $data['name'];
-            $active = $data['isActive'];
-            updateRecord($name, $active);
-        } else if (strcmp($type, "New") == 0) {
-            $code = $data['code'];
-            $desc = $data['desc'];
-            insertRecord($code, $desc);
+        switch ($type) {
+            case "Insert":
+                $code = $data['code'];
+                $desc = $data['desc'];
+                insertRecord($code, $desc);
+                break;
+            case "Update":
+                $name = $data['name'];
+                $active = $data['isActive'];
+                updateRecord($name, $active);
+                break;
+            case "Delete":
+                break;
+            default:
+                echo "error";
         }
-    
+
 } else {
    // JSON decoding failed
    http_response_code(400); // Bad Request
    echo "Invalid JSON data";
 }
-/*
-    $servername = "localhost:3306";
-    $username1 = "root";
-    $password1 = "vaXjev98";
-    $dbname = "TIMECODES";
-    $conn = new mysqli($servername,$username1,$password1,$dbname);
-    if ($conn->connect_error) {
-        die("connection failed: " . $conn->connect_error);
-    }
-    // prepare statement
-	$sql = $conn->prepare("UPDATE TIMECODES SET ISACTIVE = " . intval($active) . " WHERE TIMECODE = '" . $name . "';");
-	//$sql = $conn->prepare("UPDATE TIMECODES SET ISACTIVE = 0 WHERE TIMECODE = '100A';";
-	// execute
-	$sql->execute();
-    $conn->close();
-*/
 
 ?>
