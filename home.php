7d22503236e922bc7b67aa31d2512024bd8de0d6
mmfay <mmfay3@gmail.com>
2024-01-02 15:45:29 -0800

    Update Credential check

--- home.php
+++ home.php
@@ -1,4 +1,6 @@
 <?php
+// use this to check if there is already a session
+session_start();
 function checkCredentials($un, $pw) {
     
     $conn = getConnection();
@@ -22,7 +24,8 @@ function checkCredentials($un, $pw) {
     if (password_verify($pw,$user["PASSWORD"])) {
         $_SESSION["AUTHENTICATED"] = "TRUE";
         $_SESSION["LOGINATTEMPT"] = "TRUE";
-        $_SESSION["USERID"] = $user["USERNAME"];
+        $_SESSION["USERID"] = $un;
+        $_SESSION["PASSWORD"] = $pw;  
         return TRUE;
     } else {
         $_SESSION["AUTHENTICATED"] = "FALSE";
@@ -34,7 +37,7 @@ function checkCredentials($un, $pw) {
 }
 function printOptions() {
     // create select string
-    $sql = "SELECT TIMECODE FROM TIMECODES WHERE ISACTIVE = 1 ORDER BY TIMECODE ASC;";
+    $sql = "SELECT TIMECODE, DESCRIPTION FROM TIMECODES WHERE ISACTIVE = 1 ORDER BY TIMECODE ASC;";
 
     // prepare/execute statement
     $result = getConnection()->query($sql); 
@@ -42,7 +45,7 @@ function printOptions() {
     // loop through results and build sidenav list/links
     if($result->num_rows > 0) {
         while($row = $result->fetch_assoc()){
-            echo "<option id='" . $row["TIMECODE"] . "'>" . $row["TIMECODE"] . "</option>";
+            echo "<option id='" . $row["TIMECODE"] . "'>" . $row["TIMECODE"] . " - " . $row["DESCRIPTION"] . "</option>";
         }
     } else {
         echo "<option id='NA'>No Options</option>";
@@ -56,16 +59,14 @@ function getConnection() {
     $conn = new mysqli($servername,$username1,$password1,$dbname);
     return $conn;
 }
-// check for authenticated user global variable
-// if it doesnt exist, redirect to login page, else just write html code for authenticated user
-session_start();
-
-$un = $_REQUEST["user"];
-$pw = $_REQUEST["password"];
-if (!checkCredentials($un,$pw) && (strcmp($_SESSION["AUTHENTICATED"], "FALSE") == 0)) {
-    header('Location: login.php');
-}
-
+// check if user has authenticated, if not, check to see if this is a request and look to pass credentials
+if (!(strcmp($_SESSION["AUTHENTICATED"], "TRUE") == 0)) {
+    $un = $_REQUEST["user"];
+    $pw = $_REQUEST["password"];
+    if (!checkCredentials($un,$pw)) {
+        header('Location: login.php');
+    } 
+} 
 ?>
 <!DOCTYPE html>
 <html>
@@ -102,7 +103,6 @@ if (!checkCredentials($un,$pw) && (strcmp($_SESSION["AUTHENTICATED"], "FALSE") =
     </div>
     <script>
         function logout() {
-
             var postData = {
             title: "Logout"
             };
